ed9a5289265d72b2607ddffe15242cd0236af8d7
Taha <tahapasha121@gmail.com>
2024-06-07 01:10:25 +0530

    iwjw

--- pharmacy/src/componenets/Dashboard.js
+++ pharmacy/src/componenets/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className='bg-gradient-to-r from-purple-800 to-blue-500 w-full h-[80vh] flex-col gap-6 items-center justify-center'>
-        <div className='flex justify-between mx-5 mt-5 text-lg text-white'>
+        <div className='flex justify-between mx-5 mt-5 text-lg text-white font-bold'>
             <div>Dashboard</div>
             <div>Administrator</div>
         </div>
